39e7b1edf09763b14f68abfda829b3d314343cb2
Mohamed <looklike882@gmail.com>
2025-06-18 18:33:07 +0100

    Update JobServiceImplementation.java

--- src/main/java/org/example/app/springbootmicroservicesapi/Job/Implementation/JobServiceImplementation.java
+++ src/main/java/org/example/app/springbootmicroservicesapi/Job/Implementation/JobServiceImplementation.java
@@ -1,19 +1,20 @@
 package org.example.app.springbootmicroservicesapi.Job.Implementation;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.example.app.springbootmicroservicesapi.Job.Job;
 import org.example.app.springbootmicroservicesapi.Job.JobService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 @Service
 public class JobServiceImplementation implements JobService {
-    private List<Job> jobs  = new ArrayList<>() ;
 
-    // @Override
-    // public List<Job> getAll() {
-    //     return jobs;
-    // }
+    private List<Job> jobs = new ArrayList<>();
+
+    @Override
+    public List<Job> getAll() {
+        return jobs;
+    }
 
     @Override
     public void createJob(Job job) {
